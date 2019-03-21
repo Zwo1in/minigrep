c5c3066e0ed8498a3cc6dc5a6f30fca9bf2d3fb9
Zwo1in <zwolin13@gmail.com>
2019-03-21 21:35:48 +0100

    Redirected errors to stderr

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,14 @@ use minigrep::config::Config;
 fn main() {
     let mut args = env::args();
     let conf = Config::new(&mut args).unwrap_or_else(|err| {
-        println!("{}", err);
+        eprintln!("{}", err);
+        eprintln!("Process exit status: 1");
         process::exit(1);
     });
     
     if let Err(e) = minigrep::run(&conf) {
-        println!("Abborted due to error: {}", e);
-        println!("Process exit status: 1");
+        eprintln!("Abborted due to error: {}", e);
+        eprintln!("Process exit status: 1");
         process::exit(1);
     }
 }
